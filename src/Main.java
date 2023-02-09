e2c26375678d165b2edb0133e3087b645fe236d7
Isuru <isurudulakshana012@gmail.com>
2023-02-09 23:02:57 +0530

    update

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle("Axio", 5000000, "Corolla");
+
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,17 @@
+class Vehicle{
+    String name;
+    double price;
+    String model;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String name, double price, String model) {
+        this.name = name;
+        this.price = price;
+        this.model = model;
+    }
+    public void speed(){
+
+    }
+}
